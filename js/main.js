1cf757baf4348ccf4eb3e93bfb0ef2591baaacaf
bgcrafts <bartosz.giecewicz@gmail.com>
2021-02-10 11:34:05 +0100

    poprawione

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-firstNumber = 6 ;
-secondNumber = 8 ;
+let firstNumber = 6 ;
+let secondNumber = 8 ;
 
-function suma (firstNumber, secondNumber);
-let wynik = firstNumber + secondNumber;
-return wynik;
+function suma(a, b) {
+    let wynik = a + b;
+    return wynik;
+}
+
+console.log(suma(firstNumber, secondNumber));
+
+
